0bab09f6404b9020f1b41c3555195ab0a2c1837f
Ibrahim Jamal <ijamal68@gmail.com>
2020-09-04 18:40:47 -0600

    akfdjlasd

--- iostream.cpp
+++ iostream.cpp
@@ -4,7 +4,8 @@ int main() {
 
 
 std::cout << "hello, again, from the local machine: except this time from the folder \"another_folder\" \n" ; 
-// olo
+// olo 
+// there is no easteer bunny there is no tooth fairy there is no queen of England ... you're so naive roxanne 
 
     return 0 ; 
 };
